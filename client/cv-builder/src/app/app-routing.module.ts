434af23b2ada1aa2626de47d82839b3f08beee25
Yusuf Ridwan <horiyorrmi72@gmail.com>
2023-08-09 09:49:26 +0000

    landing nav footer

--- client/cv-builder/src/app/app-routing.module.ts
+++ client/cv-builder/src/app/app-routing.module.ts
@@ -6,9 +6,29 @@ import { FooterComponent } from './footer/footer.component';
 import { HowtoComponent } from './howto/howto.component';
 import { AboutComponent } from './about/about.component';
 import { LandingComponent } from './landing/landing.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { EduComponent } from './edu/edu.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PreviewComponent } from './preview/preview.component';
+import { BioComponent } from './bio/bio.component';
+import { ExperienceComponent } from './experience/experience.component';
+
+
+
+
 
 const routes: Routes = [
-  {path:'', component:HomeComponent}
+  {path:'', component:HomeComponent},
+  {path:"signup", component:SignupComponent},
+  {path:"login", component:LoginComponent},
+  {path:'skill', component:SkillsComponent},
+  {path:'edu', component:EduComponent},
+  {path:'preview', component:SignupComponent},
+  {path:'bio', component:BioComponent},
+  {path:'experience', component:ExperienceComponent},
+  {path:'portfolio', component:PortfolioComponent}
 ];
 
 @NgModule({
